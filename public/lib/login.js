0042c352c0d12c86f1f331426cdd62449b8a5f3d
Antosik <grig_anton96@mail.ru>
2021-01-08 15:28:45 +0300

    feat: login - discord icon

---
+++ public/lib/login.js
@@ -0,0 +1,19 @@
+/* global define, $, config */
+'use strict'
+
+$(window).on('action:script.load', function (ev, data) {
+  data.scripts.push('sso-discord-alt/login')
+})
+
+define('sso-discord-alt/login', function () {
+  const Login = {}
+
+  Login.init = function () {
+    const replaceEl = $('.alt-logins .discord a i')
+    const replacement = document.createElement('img')
+    replacement.src = config.relative_path + '/plugins/nodebb-plugin-sso-discord-alt/images/button_discord_login.png'
+    replaceEl.replaceWith(replacement)
+  }
+
+  return Login
+})
