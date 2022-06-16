1708a3f00bc2a3e4886e257a099558b42bac047d
Opliko <opliko.reg@protonmail.com>
2022-06-16 18:19:35 +0000

    go back to canonical classes

--- library.js
+++ library.js
@@ -36,7 +36,7 @@ const constants = {
   displayName: 'Discord',
   admin: {
     route: '/plugins/sso-discord-alt',
-    icon: 'fa-brands fa-discord fa-discord-icon'
+    icon: 'fab fa-discord'
   },
   oauth: { // a passport-oauth2 options object
     authorizationURL: 'https://discord.com/api/v8/oauth2/authorize',
